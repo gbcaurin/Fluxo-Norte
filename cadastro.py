2362a59f3f34af3d4de019b089fa9bac1cb2dd60
Gabriel Caurin de Souza <gabriel.cs15@puccampinas.edu.br>
2026-05-27 10:58:13 -0300

    feat: commit inicial cadastro

--- cadastro.py
+++ cadastro.py
@@ -1,1 +1,23 @@
 #Gabriel
+
+pedidos = {}
+entregador = {}
+
+def validar_id_pedido(id):
+  return 0
+
+def validar_id_entregador(id):
+  return 0
+
+def cadastro_pedido():
+  #receber os dados por iput, validar o ID e salvar no dicionario com status inicial "Pendente"
+  return 0
+
+def cadastro_entregador():
+  #receber os dados por input, validar o ID e salvar no dicionario, garante que nao tem duplicidade e inicia a lista de pedidos vazia.
+  return 0
+
+def main():
+  print("Inicio")
+
+main()
